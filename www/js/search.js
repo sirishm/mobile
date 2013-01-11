f82b4d0963e2166b95970f5e462748bc9abce652
sirish mandalika <sirish76@sirish-mandalikas-MacBook-Pro.local>
2013-01-11 09:37:08 -0800

    refinements in search

--- www/js/search.js
+++ www/js/search.js
@@ -2,6 +2,23 @@
 function searchForm()
 {
 
+var initHTML = $("<html><head><link rel=" + "stylesheet" +  "href=" + "http://code.jquery.com/mobile/1.2.0-alpha.1/jquery.mobile-1.2.0-alpha.1.min.css" + "/>" + 
+"<script src=" + "http://code.jquery.com/jquery-1.7.2.min.js" + "></script>" + 
+"<script src=" + "http://code.jquery.com/mobile/1.2.0-alpha.1/jquery.mobile-1.2.0-alpha.1.min.js" + "></script>"
++ "</head><body>"
+);
+
+print (initHTML);
+document.write(initHTML);
+
+
+var nextHTML = ("<div id=" + "rossioPage" + "data-role=" + "page" + "id=" + "album-list" + "><div data-role=" + "content" + "data-theme=" + "b" + ">" + 
+"<h1>Discounted Shoes</h1></div><!-- /header -->" + 
+"<ul id=" + "rossioList" + "data-role=" + "listview" + "data-split-icon=" + "gear" + "data-split-theme=" + "d" + "></ul></div></div>");
+
+document.write(nextHTML);
+
+
 var rossioURL = "http://www.rossiosquare.com/discount?q=bags&rows=10&format=json";
 
 var movies;
